e906145aa2919ce4315112d47006381f84ec9cff
Italo Teles <italoteles399@gmail.com>
2025-08-01 05:30:03 -0300

    feat: implementado rota countries e middleware de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
 import { userRoutes } from "./routes/users.route";
 import { errors } from "celebrate";
 import { authRoutes } from "./routes/auth.route";
+import { countriesRoutes } from "./routes/countries.route";
+import { errorHandler } from "./middlewares/error.handler";
 
 const app = express();
-
+app.use(cors());
 dotenv.config();
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
-
+app.use("/api/v1", countriesRoutes);
 app.use(errors());
+app.use(errorHandler);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log("🟢 Servidor rodando na porta", port);
